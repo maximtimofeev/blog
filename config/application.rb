650926bdfa5441cb0dd5965f7ea768dabd2a608d
maximtimofeev <makscarlos@hotmail.com>
2018-03-28 23:56:14 +0300

    homework_2 task_1

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.sass.preferred_syntax = :sass
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :posts
   root 'app#index'
 
+  get 'posts/page/(:page(.:format))', to: 'posts#index'
+  
   get 'app/index'
   get 'app/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
