f6152e1d218fbabdbdc02e25b2d3132d871bbecb
maximtimofeev <makscarlos@hotmail.com>
2018-04-06 18:49:55 +0300

    fix jquery-ujs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_scope :user do
-    get 'users/sign_out', to: 'devise/sessions#destroy'
-  end
   resources :users
   resources :posts
   root 'app#index'
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -6,7 +6,8 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-
+import "jquery";
+import "jquery-ujs";
 import 'bootstrap'
 import 'init'
 import './application.css'
